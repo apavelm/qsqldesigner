153b069cdf7998afdd4bcedd0c57d998f10fffc2
apavelm@gmail.com <apavelm@gmail.com@b976079f-fb2a-2d15-3876-d33d174e65f6>
2010-08-09 06:33:47 +0000

    some improvements

---
+++ trunk/src/commands.cpp
@@ -0,0 +1,95 @@
+#include "commands.h"
+
+MoveCommand::MoveCommand(QGraphicsItem *diagramItem, const QPointF &oldPos, QUndoCommand *parent) : QUndoCommand(parent)
+{
+    myDiagramItem = diagramItem;
+    newPos = diagramItem->pos();
+    myOldPos = oldPos;
+}
+
+bool MoveCommand::mergeWith(const QUndoCommand *command)
+{
+    const MoveCommand *moveCommand = static_cast<const MoveCommand *>(command);
+    QGraphicsItem *item = moveCommand->myDiagramItem;
+
+    if (myDiagramItem != item)
+    return false;
+
+    newPos = item->pos();
+    setText(QObject::tr("Move %1")
+        .arg(createCommandString(myDiagramItem, newPos)));
+
+    return true;
+}
+
+void MoveCommand::undo()
+{
+    myDiagramItem->setPos(myOldPos);
+    myDiagramItem->scene()->update();
+    setText(QObject::tr("Move %1")
+        .arg(createCommandString(myDiagramItem, newPos)));
+}
+
+void MoveCommand::redo()
+{
+    myDiagramItem->setPos(newPos);
+    setText(QObject::tr("Move %1")
+        .arg(createCommandString(myDiagramItem, newPos)));
+}
+
+DeleteCommand::DeleteCommand(QGraphicsScene *scene, QUndoCommand *parent) : QUndoCommand(parent)
+{
+    myGraphicsScene = scene;
+    QList<QGraphicsItem *> list = myGraphicsScene->selectedItems();
+    list.first()->setSelected(false);
+    myDiagramItem = static_cast<QGraphicsItem *>(list.first());
+    setText(QObject::tr("Delete %1")
+        .arg(createCommandString(myDiagramItem, myDiagramItem->pos())));
+}
+
+void DeleteCommand::undo()
+{
+    myGraphicsScene->addItem(myDiagramItem);
+    myGraphicsScene->update();
+}
+
+void DeleteCommand::redo()
+{
+    myGraphicsScene->removeItem(myDiagramItem);
+}
+
+AddCommand::AddCommand(int addType, QGraphicsScene *scene, QUndoCommand *parent) : QUndoCommand(parent)
+{
+    static int itemCount = 0;
+
+    myGraphicsScene = scene;
+    //myDiagramItem = new QGraphicsItem(addType);
+    initialPosition = QPointF((itemCount * 15) % int(scene->width()),
+                              (itemCount * 15) % int(scene->height()));
+    scene->update();
+    ++itemCount;
+    setText(QObject::tr("Add %1")
+        .arg(createCommandString(myDiagramItem, initialPosition)));
+}
+
+void AddCommand::undo()
+{
+    myGraphicsScene->removeItem(myDiagramItem);
+    myGraphicsScene->update();
+}
+
+void AddCommand::redo()
+{
+    myGraphicsScene->addItem(myDiagramItem);
+    myDiagramItem->setPos(initialPosition);
+    myGraphicsScene->clearSelection();
+    myGraphicsScene->update();
+}
+
+QString createCommandString(QGraphicsItem *item, const QPointF &pos)
+{
+    /*return QObject::tr("%1 at (%2, %3)")
+        .arg(item->diagramType() == DiagramItem::Box ? "Box" : "Triangle")
+        .arg(pos.x()).arg(pos.y());*/
+    return QString();
+}
---
+++ trunk/src/widgets/cornerzoomwidget.h
@@ -0,0 +1,24 @@
+#ifndef CORNERZOOMWIDGET_H
+#define CORNERZOOMWIDGET_H
+
+#include <QtGui/QStylePainter>
+#include <QtGui/QWidget>
+
+class CornerZoomWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit CornerZoomWidget(QWidget *parent = 0);
+    void setPixmap(const QPixmap& pixmap);
+    void setPixmapSize(int size);
+protected:
+   void paintEvent(QPaintEvent *);
+   void mousePressEvent(QMouseEvent *);
+private:
+    QPixmap m_pixmap;
+    int m_size;
+signals:
+    void clicked();
+};
+
+#endif // CORNERZOOMWIDGET_H
